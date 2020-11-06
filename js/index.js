06047d7297f16712f3f02726ddbe92ff555c6004
kya.rar <chiaraerre@hotmail.com>
2020-11-06 17:13:56 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,27 +2,32 @@
 
 
 function clickPelle() {
+  navigator.vibrate(0);
   $("#vibrolino").text("pellino");
   navigator.vibrate([1000]);
 }
 
 function clickCapelli() {
+  navigator.vibrate(0);
   $("#vibrolino").text("capellino");
   navigator.vibrate(Infinity);
 }
 
 function clickVestito() {
+  navigator.vibrate(0);
   $("#vibrolino").text("vestitino");
   navigator.vibrate("long");
 }
 
 function clickSfondo() {
+  navigator.vibrate(0);
   $("#vibrolino").text("sfondino");
   navigator.vibrate("long");
 }
 
 
 function clickCielo() {
+  navigator.vibrate(0);
   $("#vibrolino").text("cielino");
   navigator.vibrate("long");
 }
