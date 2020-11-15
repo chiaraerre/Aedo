f077842364f8cc05b9ce32997b678f2acb911016
kya.rar <chiaraerre@hotmail.com>
2020-11-15 14:53:34 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ ctx.fillStyle = 'red';
 ctx.fill(circle);
 
 // Listen for mouse moves
-canvas.addEventListener('mousemove', function(event) {
+canvas.addEventListener('touchmove', function(event) {
   // Check whether point is inside circle
   if (ctx.isPointInPath(circle, event.offsetX, event.offsetY)) {
     ctx.fillStyle = 'green';
